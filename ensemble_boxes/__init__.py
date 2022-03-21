370fb94bbbb1d0f9f107178ccc7b90d5ade76706
Роман <turbo@ippm.ru>
2022-03-21 20:32:35 +0300

    Added experimental version to imports

--- ensemble_boxes/__init__.py
+++ ensemble_boxes/__init__.py
@@ -8,3 +8,4 @@ from .ensemble_boxes_nms import nms
 from .ensemble_boxes_nms import soft_nms
 from .ensemble_boxes_wbf_3d import weighted_boxes_fusion_3d
 from .ensemble_boxes_wbf_1d import weighted_boxes_fusion_1d
+from .ensemble_boxes_wbf_experimental import weighted_boxes_fusion_experimental
