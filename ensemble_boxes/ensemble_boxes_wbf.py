43505623f67a088e7cec42191c494d4b47dabefe
RemiFabre <remifabre1800@gmail.com>
2023-01-10 18:13:10 +0100

    Adding support for 3D (X,Y,Z) position

--- ensemble_boxes/ensemble_boxes_wbf.py
+++ ensemble_boxes/ensemble_boxes_wbf.py
@@ -6,18 +6,20 @@ import warnings
 import numpy as np
 
 
-def prefilter_boxes(boxes, scores, labels, weights, thr):
+def prefilter_boxes(boxes, scores, labels, positions_list, weights, thr):
     # Create dict with boxes stored by its label
     new_boxes = dict()
 
     for t in range(len(boxes)):
 
         if len(boxes[t]) != len(scores[t]):
-            print('Error. Length of boxes arrays not equal to length of scores array: {} != {}'.format(len(boxes[t]), len(scores[t])))
+            print('Error. Length of boxes arrays not equal to length of scores array: {} != {}'.format(
+                len(boxes[t]), len(scores[t])))
             exit()
 
         if len(boxes[t]) != len(labels[t]):
-            print('Error. Length of boxes arrays not equal to length of labels array: {} != {}'.format(len(boxes[t]), len(labels[t])))
+            print('Error. Length of boxes arrays not equal to length of labels array: {} != {}'.format(
+                len(boxes[t]), len(labels[t])))
             exit()
 
         for j in range(len(boxes[t])):
@@ -30,6 +32,11 @@ def prefilter_boxes(boxes, scores, labels, weights, thr):
             y1 = float(box_part[1])
             x2 = float(box_part[2])
             y2 = float(box_part[3])
+
+            # x, y, and z positions
+            x = float(positions_list[t][j][0])
+            y = float(positions_list[t][j][1])
+            z = float(positions_list[t][j][2])
 
             # Box data checks
             if x2 < x1:
@@ -66,8 +73,8 @@ def prefilter_boxes(boxes, scores, labels, weights, thr):
                 warnings.warn("Zero area box skipped: {}.".format(box_part))
                 continue
 
-            # [label, score, weight, model index, x1, y1, x2, y2]
-            b = [int(label), float(score) * weights[t], weights[t], t, x1, y1, x2, y2]
+            # [label, score, weight, model index, x1, y1, x2, y2, x, y, z]
+            b = [int(label), float(score) * weights[t], weights[t], t, x1, y1, x2, y2, x, y, z]
             if label not in new_boxes:
                 new_boxes[label] = []
             new_boxes[label].append(b)
@@ -85,15 +92,16 @@ def get_weighted_box(boxes, conf_type='avg'):
     Create weighted box for set of boxes
     :param boxes: set of boxes to fuse
     :param conf_type: type of confidence one of 'avg' or 'max'
-    :return: weighted box (label, score, weight, model index, x1, y1, x2, y2)
+    :return: weighted box (label, score, weight, model index, x1, y1, x2, y2, x, y, z)
     """
 
-    box = np.zeros(8, dtype=np.float32)
+    box = np.zeros(11, dtype=np.float32)
     conf = 0
     conf_list = []
     w = 0
     for b in boxes:
-        box[4:] += (b[1] * b[4:])
+        box[4:8] += (b[1] * b[4:8])
+        box[8:11] += (b[1] * b[8:11])
         conf += b[1]
         conf_list.append(b[1])
         w += b[2]
@@ -103,8 +111,9 @@ def get_weighted_box(boxes, conf_type='avg'):
     elif conf_type == 'max':
         box[1] = np.array(conf_list).max()
     box[2] = w
-    box[3] = -1 # model index field is retained for consistency but is not used.
-    box[4:] /= conf
+    box[3] = -1  # model index field is retained for consistency but is not used.
+    box[4:8] /= conf
+    box[8:11] /= conf
     return box
 
 
@@ -136,7 +145,7 @@ def find_matching_box_fast(boxes_list, new_box, match_iou):
     # boxes = np.array(boxes_list)
     boxes = boxes_list
 
-    ious = bb_iou_array(boxes[:, 4:], new_box[4:])
+    ious = bb_iou_array(boxes[:, 4:8], new_box[4:8])
 
     ious[boxes[:, 0] != new_box[0]] = -1
 
@@ -154,6 +163,7 @@ def weighted_boxes_fusion(
         boxes_list,
         scores_list,
         labels_list,
+        positions_list,
         weights=None,
         iou_thr=0.55,
         skip_box_thr=0.0,
@@ -166,6 +176,7 @@ def weighted_boxes_fusion(
     Order of boxes: x1, y1, x2, y2. We expect float normalized coordinates [0; 1]
     :param scores_list: list of scores for each model
     :param labels_list: list of labels for each model
+    :param positions_list: list of (x, y, z) positions for each model
     :param weights: list of weights for each model. Default: None, which means weight == 1 for each model
     :param iou_thr: IoU value for boxes to be a match
     :param skip_box_thr: exclude boxes with score lower than this variable
@@ -179,8 +190,8 @@ def weighted_boxes_fusion(
     :return: boxes: boxes coordinates (Order of boxes: x1, y1, x2, y2).
     :return: scores: confidence scores
     :return: labels: boxes labels
+    :return: positions: (x, y, z) positions
     '''
-
     if weights is None:
         weights = np.ones(len(boxes_list))
     if len(weights) != len(boxes_list):
@@ -192,15 +203,15 @@ def weighted_boxes_fusion(
         print('Unknown conf_type: {}. Must be "avg", "max" or "box_and_model_avg", or "absent_model_aware_avg"'.format(conf_type))
         exit()
 
-    filtered_boxes = prefilter_boxes(boxes_list, scores_list, labels_list, weights, skip_box_thr)
+    filtered_boxes = prefilter_boxes(boxes_list, scores_list, labels_list, positions_list, weights, skip_box_thr)
     if len(filtered_boxes) == 0:
-        return np.zeros((0, 4)), np.zeros((0,)), np.zeros((0,))
-
+        return np.zeros((0, 4)), np.zeros((0,)), np.zeros((0,)), np.zeros((0, 3))
+    # positions_list update fait jusqu'ici.
     overall_boxes = []
     for label in filtered_boxes:
         boxes = filtered_boxes[label]
         new_boxes = []
-        weighted_boxes = np.empty((0, 8))
+        weighted_boxes = np.empty((0, 11))
 
         # Clusterize boxes
         for j in range(0, len(boxes)):
@@ -223,7 +234,7 @@ def weighted_boxes_fusion(
                 # identify unique model index by model index column
                 _, idx = np.unique(clustered_boxes[:, 3], return_index=True)
                 # rescale by unique model weights
-                weighted_boxes[i, 1] = weighted_boxes[i, 1] *  clustered_boxes[idx, 2].sum() / weights.sum()
+                weighted_boxes[i, 1] = weighted_boxes[i, 1] * clustered_boxes[idx, 2].sum() / weights.sum()
             elif conf_type == 'absent_model_aware_avg':
                 clustered_boxes = np.array(clustered_boxes)
                 # get unique model index in the cluster
@@ -232,7 +243,8 @@ def weighted_boxes_fusion(
                 mask = np.ones(len(weights), dtype=bool)
                 mask[models] = False
                 # absent model aware weighted average
-                weighted_boxes[i, 1] = weighted_boxes[i, 1] * len(clustered_boxes) / (weighted_boxes[i, 2] + weights[mask].sum())
+                weighted_boxes[i, 1] = weighted_boxes[i, 1] * \
+                    len(clustered_boxes) / (weighted_boxes[i, 2] + weights[mask].sum())
             elif conf_type == 'max':
                 weighted_boxes[i, 1] = weighted_boxes[i, 1] / weights.max()
             elif not allows_overflow:
@@ -242,7 +254,10 @@ def weighted_boxes_fusion(
         overall_boxes.append(weighted_boxes)
     overall_boxes = np.concatenate(overall_boxes, axis=0)
     overall_boxes = overall_boxes[overall_boxes[:, 1].argsort()[::-1]]
-    boxes = overall_boxes[:, 4:]
+    boxes = overall_boxes[:, 4:8]
     scores = overall_boxes[:, 1]
     labels = overall_boxes[:, 0]
-    return boxes, scores, labels
+    positions = overall_boxes[:, 8:]
+    # [label, score, weight, model index, x1, y1, x2, y2, x, y, z]
+
+    return boxes, scores, labels, positions
